75cadc27fbe697df241e68bc9c25153d4ff7594f
BRADDTELES <braddteles@gmail.com>
2025-01-23 18:02:24 -0300

    Widgets - Stateless & Stateful

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,19 +3,74 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
+    home: HomeStateful(),
+  ));
+}
+
+class HomeStateful extends StatefulWidget {
+  const HomeStateful({super.key});
+
+  @override
+  State<HomeStateful> createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+
+  var _texto = "Jamilton Damasceno";
+
+  @override
+  Widget build(BuildContext context) {
+
+    print("Build chamado");
+
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Instagram"),
+        title: Text( "Instagram" ),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            ElevatedButton(
+                onPressed: (){
+                  setState(() {
+                    _texto = "Curso Flutter";
+                  });
+                },
+                style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.amber)),
+                child: const Text("Clique aqui")
+            ),
+            Text("Nome: $_texto")
+          ],
+        ),
+      ),
+    );
+
+  }
+}
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    var _titulo = "Instagram";
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text( _titulo ),
         backgroundColor: Colors.green,
       ),
       body: Padding(
-          padding: EdgeInsets.all(16),
+        padding: EdgeInsets.all(16),
         child: Text("Conteúdo principal"),
       ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.lightGreen,
         child: Padding(
-            padding: EdgeInsets.all(16),
+          padding: EdgeInsets.all(16),
           child: Row(
             children: [
               Text("Texto 1"),
@@ -24,6 +79,8 @@ void main() {
           ),
         ),
       ),
-    ) ,
-  ));
+    );
+
+  }
 }
+
