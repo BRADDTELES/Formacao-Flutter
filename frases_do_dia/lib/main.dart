1c7a1b285e45d2ae43579b5cd9b39f0e00e3fc6d
BRADDTELES <braddteles@gmail.com>
2025-01-23 14:18:00 -0300

    Formatação de textos

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -4,12 +4,27 @@ void main() {
 
   runApp(new MaterialApp(
     title: "Frases do dia",
-    home: Row(children: [
-      Text(" t1"),
-      Text(" t2"),
-      Text(" t3"),
-    ],),
-    //home: Container(color: Colors.white,),
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Text(
+              "Lorem ipsum ",
+            style: TextStyle(
+              fontSize: 50,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.normal,
+              letterSpacing: 0,
+              wordSpacing: 0,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+              decorationColor: Colors.greenAccent,
+              color: Colors.black
+            ),
+          ),
+        ],
+      ),
+    ),
   ));
 
 }
