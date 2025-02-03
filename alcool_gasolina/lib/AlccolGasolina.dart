5c83789121f6c6d6985c891dcbb03ddf74ac2fd6
BRADDTELES <braddteles@gmail.com>
2025-02-03 06:43:06 -0300

    #App3 Álcool ou Gasolina - criando interface - SingleChildScrollView, ElevatedButton, crossAxisAlignment: CrossAxisAlignment.stretch,

--- alcool_gasolina/lib/AlccolGasolina.dart
+++ alcool_gasolina/lib/AlccolGasolina.dart
@@ -8,6 +8,10 @@ class AlcoolGasolina extends StatefulWidget {
 }
 
 class _AlcoolGasolinaState extends State<AlcoolGasolina> {
+
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,36 +20,65 @@ class _AlcoolGasolinaState extends State<AlcoolGasolina> {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.all(32)),
-            Image.asset("imagens/logo.png"),
-            Text(
-              "Saiba qual a melhor opção para abastecimento do seu carro",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Preço Álcool, ex: 1.59"
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("imagens/logo.png"),
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  labelText: "Preço Gasolina, ex: 3.15"
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecimento do seu carro",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
-            ),
-            TextButton(
-              onPressed: (){},
-              child: Text(
-                "Calcular",
-                style: TextStyle(color: Colors.white),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Álcool, ex: 1.59"
+                ),
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+                controller: _controllerAlcool,
               ),
-              style: TextButton.styleFrom(backgroundColor: Colors.blue),
-            ),
-          ],
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Gasolina, ex: 3.15"
+                ),
+                style: TextStyle(
+                  fontSize: 22,
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                  onPressed: (){},
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
