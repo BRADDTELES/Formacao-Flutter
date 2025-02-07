6479b052572266185e4982a594ccdbfa903f4460
BRADDTELES <braddteles@gmail.com>
2025-02-07 07:04:04 -0300

    Passando dados entre telas

--- navecacao/lib/TelaSecundaria.dart
+++ navecacao/lib/TelaSecundaria.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TelaSecundaria extends StatefulWidget {
-  const TelaSecundaria({super.key});
+
+  String valor;
+
+  TelaSecundaria(this.valor,{super.key});
 
   @override
   State<TelaSecundaria> createState() => _TelaSecundariaState();
 }
 
 class _TelaSecundariaState extends State<TelaSecundaria> {
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +25,7 @@ class _TelaSecundariaState extends State<TelaSecundaria> {
         padding: EdgeInsets.all(32),
         child: Column(
           children: [
-            Text('Segunda tela!!!!'),
+            Text('Segunda tela! valor passado: ${this.widget.valor}'),
           ],
         ),
       ),
