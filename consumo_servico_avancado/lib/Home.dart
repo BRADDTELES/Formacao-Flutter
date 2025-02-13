016e86d6c05424f10c062300cb4a87c02596ac12
BRADDTELES <braddteles@gmail.com>
2025-02-13 07:21:29 -0300

    Carregando dados em uma lista (ListView)

--- consumo_servico_avancado/lib/Home.dart
+++ consumo_servico_avancado/lib/Home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:consumo_servico_avancado/Post.dart';
 import 'dart:convert';
 import 'dart:async';
+
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,46 +14,84 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  Future<Map>? _recuperarPreco() async {
-    var url = Uri.parse("https://blockchain.info/ticker");
-    http.Response response = await http.get(url);
-    return json.decode( response.body );
+  final _urlBase = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+  //String _urlBase = "https://jsonplaceholder.typicode.com/";// NÃO FUNCIONOU
+
+  Future<List<Post>> _recuperarPostagens() async {
+    //var _urlBase = Uri.parse("https://jsonplaceholder.typicode.com/posts");
+    http.Response response = await http.get( _urlBase );
+    var dadosJson = json.decode( response.body );
+
+    List<Post> postagens = [];
+    for( var post in dadosJson ){
+
+      print("${"post: " + post["title"]} ${post["id"]}" );
+      Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
+      postagens.add( p );
+
+    }
+    return postagens;
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-      future: _recuperarPreco(),
-      builder: (context, snapshot){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Consumo de serviço avançado",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: _recuperarPostagens(),
+        builder: (context, snapshot){
 
-        String resultado = "";
-        switch( snapshot.connectionState ){
-          case ConnectionState.none :
-            print("conexao none");
-            break;
-          case ConnectionState.waiting :
-            print("conexao waiting");
-            resultado = "Carregando...";
-            break;
-          case ConnectionState.active :
-            print("conexao active");
-            break;
-          case ConnectionState.done :
-            print("conexao done");
-            if ( snapshot.hasError ){
-              resultado = "Erro ao carregar os dados.";
-            }else{
+          switch( snapshot.connectionState ){
+            case ConnectionState.none :
+              print("conexao none");
+              break;
+            case ConnectionState.waiting :
+              print("conexao waiting");
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
 
-              double valor = snapshot.data?["BRL"]["buy"];
-              resultado = "Preço do bitcoin: ${valor.toString()} ";
+            case ConnectionState.active :
+              print("conexao active");
+              break;
+            case ConnectionState.done :
+              print("conexao done");
+              if ( snapshot.hasError ){
+                print("lista: Erro ao carregar ");
+              }else {
 
-            }
-            break;
-        }
-        return Center(
-          child: Text( resultado ),
-        );
-      },
+                print("lista dentro do else: carregou!! ");
+                return ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index){
+
+                    List<Post>? lista = snapshot.data;
+                    Post post = lista![index];
+
+                    return ListTile(
+                      title: Text( post.title ),
+                      subtitle: Text( post.id.toString() ),
+                    );
+
+                  }
+                );
+
+              }
+          }
+
+          return const Center(
+            child: Text("Não gerou a lista da API"),
+          );
+
+        },
+      ),
     );
   }
 }
