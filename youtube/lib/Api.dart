d8e7e54e726af539a4a5d69131d8deb7783124fa
BRADDTELES <braddteles@gmail.com>
2025-02-16 16:04:22 -0300

    #App7 Youtube - Convertendo resultados

--- youtube/lib/Api.dart
+++ youtube/lib/Api.dart
@@ -1,6 +1,8 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:youtube/model/Video.dart';
+
 const CHAVE_YOUTUBE_API = "AIzaSyBS_aWCLwrkUuGWPUmRPpQv6ek2yXcEXn4";
 const ID_CANAL = "UCVHFbqXqoYvEWM1Ddxl0QDg";
 const URL_BASE = "https://www.googleapis.com/youtube/v3/";
@@ -25,8 +27,23 @@ class Api {
     if ( response.statusCode == 200 ){
 
       Map<String, dynamic> dadosJson = json.decode( response.body );
-      //print("resultado: " + dadosJson["items"][0]["id"]["videoId"].toString() );
-      print("resultado: " + dadosJson["items"][1]["snippet"]["title"].toString() );
+
+      List<Video> videos = dadosJson["items"].map<Video>(
+        (map){
+          //return Video.converterJson(map);
+          return Video.fromJson(map);
+        }
+      ).toList();
+
+      for( var video in videos ){
+        print("resultado: " + video.titulo.toString() );
+      }
+      //print("Resultado: " + videos.toString());
+
+      /*for( var video in dadosJson["items"] ) {
+        print("resultado: " + video.toString() );
+      }*/
+      //print("resultado: " + dadosJson["items"][0]["snippet"]["title"].toString() );
 
     }else{
       
