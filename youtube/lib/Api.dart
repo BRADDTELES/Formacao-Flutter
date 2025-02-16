bb47976a50cc8f100a7a89ac180a3b5bf9c7fbc7
BRADDTELES <braddteles@gmail.com>
2025-02-16 14:39:52 -0300

    #App7 Youtube - Recuperando vídeos - Desafio feito

--- youtube/lib/Api.dart
+++ youtube/lib/Api.dart
@@ -1,3 +1,37 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const CHAVE_YOUTUBE_API = "AIzaSyBS_aWCLwrkUuGWPUmRPpQv6ek2yXcEXn4";
-const ID_CANAL = "@AndroidDevelopers";
+const ID_CANAL = "UCVHFbqXqoYvEWM1Ddxl0QDg";
 const URL_BASE = "https://www.googleapis.com/youtube/v3/";
+
+class Api {
+
+  pesquisar(String pesquisa) async {
+
+    http.Response response = await http.get(
+        Uri.parse(
+            URL_BASE + "search"
+                "?part=snippet"
+                "&type=video"
+                "&maxResults=20"
+                "&order=date"
+                "&key=$CHAVE_YOUTUBE_API"
+                "&channelId=$ID_CANAL"
+                "&q=$pesquisa"
+        )
+    );
+    
+    if ( response.statusCode == 200 ){
+
+      Map<String, dynamic> dadosJson = json.decode( response.body );
+      //print("resultado: " + dadosJson["items"][0]["id"]["videoId"].toString() );
+      print("resultado: " + dadosJson["items"][0]["snippet"]["title"].toString() );
+
+    }else{
+      
+    }
+
+  }
+
+}
