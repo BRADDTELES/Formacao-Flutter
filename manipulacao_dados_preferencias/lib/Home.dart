567aada65c6fbc6f24bba62618f7746c2c6d541b
BRADDTELES <braddteles@gmail.com>
2025-02-19 16:09:58 -0300

    Utilizando preferências - parte 2

--- manipulacao_dados_preferencias/lib/Home.dart
+++ manipulacao_dados_preferencias/lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,12 +10,36 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  String _textoSalvo = "Nada Salvo!";
   TextEditingController _controllerCampo = TextEditingController();
 
-  _salvar(){
+  _salvar() async {
+
+    String valorDigitado = _controllerCampo.text;
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString( "nome", valorDigitado );
+    
+    print("operacao (salvar): $valorDigitado");
 
   }
-  _recuperar(){
+  _recuperar() async {
+
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _textoSalvo = prefs.getString("nome") ?? "Sem valor";
+    });
+
+    print("operacao (recuperar): $_textoSalvo");
+
+  }
+
+  _remover() async {
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove("nome");
+
+    print("operacao (remover)");
 
   }
 
@@ -31,7 +56,7 @@ class _HomeState extends State<Home> {
         alignment: Alignment.center,
         child: Column(
           children: [
-            Text("Nada salvo!", style: TextStyle(fontSize: 20),),
+            Text(_textoSalvo, style: TextStyle(fontSize: 20),),
             TextField(
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
@@ -49,7 +74,7 @@ class _HomeState extends State<Home> {
                 ElevatedButton(
                   onPressed: _salvar,
                   child: Text("Salvar",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                    style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
@@ -57,11 +82,21 @@ class _HomeState extends State<Home> {
                       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
                   ),
                 ),
-                Padding(padding: EdgeInsets.only(right: 5)),
                 ElevatedButton(
                   onPressed: _recuperar,
                   child: Text("Recuperar",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _remover,
+                  child: Text("Remover",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
                   ),
                   style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
