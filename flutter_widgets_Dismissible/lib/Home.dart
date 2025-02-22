c343574dd942a4383dff78e1df7c78835c956099
BRADDTELES <braddteles@gmail.com>
2025-02-22 06:47:37 -0300

    #App8 Lista de tarefas - Utilizando Dismissible widget - Usando o Dismissible

--- flutter_widgets_Dismissible/lib/Home.dart
+++ flutter_widgets_Dismissible/lib/Home.dart
@@ -24,8 +24,52 @@ class _HomeState extends State<Home> {
             child: ListView.builder(
               itemCount: _lista.length,
               itemBuilder: (context, index){
-                return ListTile(
-                  title: Text( _lista[index] ),
+                final item = _lista[index];
+                return Dismissible(
+                  background: Container(
+                    color: Colors?.green,
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment?.start,
+                      children: [
+                        Icon(
+                            Icons.edit,
+                          color: Colors?.white,
+                        )
+                      ],
+                    ),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors?.red,
+                    padding: EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment?.end,
+                      children: [
+                        Icon(
+                          Icons.delete,
+                          color: Colors?.white,
+                        )
+                      ],
+                    ),
+                  ),
+                  //direction: DismissDirection?.horizontal, //horizontal por padrão
+                  onDismissed: (direction){
+
+                    if(direction == DismissDirection?.endToStart){
+                      print("direcao: endToStart");
+                    }else if(direction == DismissDirection.startToEnd){
+                      print("direcao: startToEnd");
+                    }
+
+                    setState(() {
+                      _lista.removeAt(index);
+                    });
+
+                  },
+                    key: Key( item ),
+                    child: ListTile(
+                      title: Text( item ),
+                    )
                 );
               },
             ),
